bd9427066fd945ec8521646755b9ad9c1718f320
Steve <sturner1123@gmail.com>
2022-03-05 16:11:15 -0500

    Updated control algorithm and moved test files

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,11 @@
 /* 
     The purpose of this program is to generate gcode based on a text input that describes the intended output.
 
-    It is broken into a few setions, the first parses the input and turns it into a weighted graph
-
-    The second takes that weighted graph and finds the best path, using dijkstra's
-
-    The third genreates the output gcode
+    The program will snake through the given input to produce gcode output.
 */
 
-use utils::read_file;
-use std::io::BufRead;  
+use std::env;
+use utils::read_to_str_vec;
 
 struct LaserCutter {
     x_pos: f32,
@@ -44,19 +40,19 @@ impl LaserCutter {
     // toggles the laser state, prints gcode, and adds to the time
     fn toggle_laser(&mut self){
         self.laser_state = !self.laser_state;
-        print!("M01\n");
+        println!("M01");
         //turning on/off the laser takes 1 second
         self.total_time += 1.0;
     }
 
     //displays the current total time in a pretty format, to be used when gcode is finished being generated.
     fn display_time(&self){
-        print!("This Gcode should take {:.1} seconds to run", self.total_time);
+        println!("This Gcode should take {:.1} seconds to run", self.total_time);
     }
 
     //small function to output gcode of current position
     fn output_pos_gcode(&self){
-        print!("G01 X{:.2} Y{:.2}\n", self.x_pos, self.y_pos);
+        println!("G01 X{:.2} Y{:.2}", self.x_pos, self.y_pos);
     }
 
     //function to calculate the distance between 2 points
@@ -68,16 +64,99 @@ impl LaserCutter {
 }
 
 fn main() {
-  let mut laser = LaserCutter::new();
-  laser.set_pos(2.0,1.0);
-  laser.toggle_laser();
-  laser.set_pos(6.0,1.0);
-  laser.toggle_laser();
-  laser.set_pos(2.0,3.0);
-  laser.toggle_laser();
-  laser.set_pos(6.0,3.0);
-  laser.toggle_laser();
-  laser.set_pos(0.0,0.0);
-  laser.display_time()
+    // create our laser cutter to perform actions on
+    let mut laser = LaserCutter::new();
+    // get text file name from command line
+    let args: Vec<String> = env::args().collect();
+    
+    //only takes one command line arg (executing file name is automatically included)
+    assert!(args.len() == 2);
+
+    let input = read_to_str_vec(args[1].as_str());
+    for i in 0..input.len() {
+        if (i % 2) == 0 {
+            for c in input[i].char_indices() {
+                if c.1 == 'X' && !laser.laser_state{
+                    laser.set_pos(i as f32, c.0 as f32);
+                    laser.toggle_laser();
+                }
+                else if c.1 == 'X' && laser.laser_state {
+                    //just move to position
+                    laser.set_pos(i as f32, c.0 as f32);
+                }
+                else if laser.laser_state{
+                    //if on, turn off
+                    laser.toggle_laser();
+                }
+            }
+        }
+        else {
+            for c in input[i].char_indices().rev() {
+                if c.1 == 'X' && !laser.laser_state{
+                    laser.set_pos(i as f32, c.0 as f32);
+                    laser.toggle_laser();
+                }
+                else if c.1 == 'X' && laser.laser_state {
+                    //just move to position
+                    laser.set_pos(i as f32, c.0 as f32);
+                }
+                else if laser.laser_state{
+                    //if on, turn off
+                    laser.toggle_laser();
+                }
+            }
+        }
+    }
+    //we've parsed all the gcode, home the laser
+    laser.set_pos(0.0,0.0);
+    laser.display_time();
 }
 
+mod tests{
+    use super::LaserCutter;
+
+    #[test]
+    //this test checks that the distance calculation is correct
+    fn test_calc_distance() {
+        let mut laser = LaserCutter::new();
+        assert_eq!(laser.calculate_distance(0.0,0.0,3.0,4.0), 5.0);
+    }
+
+    #[test]
+    //this test checks that set pos updates correctly
+    fn test_set_pos() {
+        let mut laser = LaserCutter::new();
+        laser.set_pos(5.0, 3.0);
+        assert_eq!(laser.x_pos, 5.0);
+        assert_eq!(laser.y_pos, 3.0);
+        assert_eq!(laser.total_time, 34.0_f32.sqrt());
+        
+    }
+
+    #[test]
+    //this test checks that toggling the laser will add to the total time
+    fn test_laser_toggle() {
+        let mut laser = LaserCutter::new();
+        laser.toggle_laser();
+        assert!(laser.laser_state == true);
+        assert_eq!(laser.total_time, 1.0);
+    }
+
+    #[test]
+    //this test will test a full run of gcode to test the total time.
+    fn test_full_run() {
+        let mut laser = LaserCutter::new();
+        laser.set_pos(2.0,1.0);
+        laser.toggle_laser();
+        laser.set_pos(6.0,1.0);
+        laser.toggle_laser();
+        laser.set_pos(2.0,3.0);
+        laser.toggle_laser();
+        laser.set_pos(6.0,3.0);
+        laser.toggle_laser();
+        laser.set_pos(0.0,0.0);
+        
+        let time = (laser.total_time * 100.0).round() / 100.0;
+        assert_eq!(time, 25.42);
+    }
+}
